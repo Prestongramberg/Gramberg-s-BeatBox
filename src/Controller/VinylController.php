bcc724dcb1453ec3296c9c0c5dceabddad6e369c
prestongramberg <knighttraintint@gmail.com>
2024-03-02 16:55:05 -0600

    Add: Homepage & Browse Routes

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,21 +1,29 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use function Symfony\Component\String\u;
+
 class VinylController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-       return new Response('Title: Gramberg\'s BeatBox');
+        return new Response('Title: Gramberg\'s BeatBox');
     }
 
-    #[Route('/browse')]
-    public function browse() :Response
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
     {
-        return new Response('Breakup vintl? Angsty 90s rock? Browse the collection!');
+        if ($slug) {
+            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+
+        return new Response($title);
     }
-
-
 }
