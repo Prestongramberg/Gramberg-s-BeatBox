f638435752e4806fa7724069ca0d2baf221edf4d
prestongramberg <knighttraintint@gmail.com>
2024-03-19 16:44:08 -0500

    Add: Auto wiring enabled

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,23 +3,26 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class MixRepository
+readonly class MixRepository
 {
 
     public function __construct(
-        private HttpClientInterface $httpClient,
-        private CacheInterface $cache
+        private HttpClientInterface $githubContentClient,
+        private CacheInterface $cache,
+        #[Autowire('&kernel.debug&')]
+        private bool $isDebug
     ) {
     }
 
     public function findAll(): array
     {
         return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
-            $cacheItem->expiresAfter(5);
-            $response = $this->httpClient->request(
+            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
+            $response = $this->githubContentClient->request(
                 'GET',
                 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json'
             );
